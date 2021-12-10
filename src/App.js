dd07739ad6ca882282bcac7a0790ef28e2c80be6
kokoaono <nyswzt.a@gmail.com>
2021-12-10 20:40:27 +1300

    foodtemp form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FridgeTempForm from './components/FridgeTempForm';
 import Home from './components/home/Home';
 import Header from './components/Header';
+import FoodTempForm from './components/FoodTempForm';
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='temp' element={<FridgeTempForm />} />
+          <Route path='foodtemp' element={<FoodTempForm />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/FoodTempForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+
+const FoodTempForm = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [foodTemp, setFoodTemp] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFoodTemp(e.target.vale)
+    console.log(e.target.value);
+  };
+
+
+  return (
+    <div>
+      <form>
+        <select>
+          <option defaultValue="Protein">Select protein</option>
+          <option value="Beef">Beef</option>
+          <option value="Chicken">Chicken</option>
+          <option value="Lamb">Lamb</option>
+          <option value="Pork">Pork</option>
+        </select>
+        <input
+          type="number"
+          value={foodTemp}
+          onChange={handleChange}
+        >
+
+        </input>
+
+      </form>
+      <DatePicker
+        dateFormat="dd/MM/yyyy h:mm aa"
+        timeInputLabel='Time:'
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+      />
+    </div>
+  )
+};
+
+export default FoodTempForm;
