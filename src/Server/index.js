ebba3f55ac4e8146d8c956c8ceb908fd6015d1f8
kokoaono <nyswzt.a@gmail.com>
2022-02-16 11:13:32 +1300

    working on express

--- src/Server/index.js
+++ src/Server/index.js
@@ -2,6 +2,10 @@ const server = require('./server')
 
 const PORT = process.env.PORT || 3000
 
+server.get('/', (req, res) => {
+  res.send('hello')
+})
+
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 });
--- src/Server/server.js
+++ src/Server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const server = express();
+
+server.use(express.static("public"))
+server.use(express.urlencoded({ extended: true }))
 
 module.exports = server
