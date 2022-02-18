b7b6b60b0ad34460f0289dd841d51bcf64ba3f65
kokoaono <nyswzt.a@gmail.com>
2022-02-18 16:48:27 +1300

    sqlite

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const server = express();
 
+
+
 const PORT = process.env.PORT || 5000
+
+
 
 server.use(express.json())
 
-const itemsRouter = require('./Routes/items')
 
-server.use('/items', itemsRouter)
 
+// const itemsRouter = require('./routes/items');
+// server.use('/items', itemsRouter);
 
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
