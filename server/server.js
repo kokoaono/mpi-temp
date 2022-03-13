e843c04f574a0640fd4e489273e88b82b99fadac
kokoaono <nyswzt.a@gmail.com>
2022-03-13 21:09:33 +1300

    fix CORS error

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const server = express()
 server.use(express.static(path.join(__dirname, './../public')));
 server.use(express.json());
 // server.use(express.urlencoded({ extended: false }))
+server.get('/items', (req, res) => {
+  res.header("Access-control-Allow-Origin", "*")
+  res.send('hello world')
+})
 
 const itemsRouter = require('./routes/items');
 const fridgeRouter = require('./routes/fridges');
