7895751ad0bf990f522a32f96541f3ee5c394f1f
kokoaono <nyswzt.a@gmail.com>
2022-05-05 16:41:01 +1200

    testing react-query

--- src/components/Food/FoodApp.js
+++ src/components/Food/FoodApp.js
@@ -1,34 +1,51 @@
-import React, { useState, useEffect } from 'react';
-// import { useQuery } from 'react-query';
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import { FoodItems } from './FoodItems';
 import { AddItem } from './AddItem';
 import { FoodHeader } from './FoodHeader';
 import api from '../../api/api';
 import { Flex, Box } from '@chakra-ui/react';
 
+//create a queryClient object
+const queryClient = new QueryClient();
+
 export const FoodApp = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Dummy />
+    </QueryClientProvider>
+  )
+};
+
+function Dummy() {
   const [items, setItems] = useState([])
   const [showAddItem, setShowAddItem] = useState(false);
+  //create a component that uses useQuery
+  const queryItems = useQuery('items', () => api.get('/items').then(res => res.data)
+  )
+  console.log('query log', queryItems)
 
-  // const query = useQuery('items', () => { return api.get('/items').then(res => res.data) })
 
-  const getItems = async () => {
-    const response = await api.get('/items');
-    return response.data.items
-  };
 
-  useEffect(() => {
-    const getAllItems = async () => {
-      try {
-        const response = await getItems();
-        console.log(response);
-        if (response) setItems(response);
-      } catch (err) {
-        console.log("Error", err.message);
-      }
-    }
-    getAllItems()
-  }, [])
+
+
+  // const getItems = async () => {
+  //   const response = await api.get('/items');
+  //   return response.data.items
+  // };
+
+  // useEffect(() => {
+  //   const getAllItems = async () => {
+  //     try {
+  //       const response = await getItems();
+  //       console.log(response);
+  //       if (response) setItems(response);
+  //     } catch (err) {
+  //       console.log("Error", err.message);
+  //     }
+  //   }
+  //   getAllItems()
+  // }, [])
 
   return (
     <Box mx={5}>
